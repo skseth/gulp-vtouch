673949f12245a2e2e7f4c5f4aad3187cc7ca2b54
Samir Seth <samirkseth@gmail.com>
2016-01-03 14:12:38 +0530

    first check-in

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # gulp-vtouch
-gulp plugin to change modified time of file, based on dependencies
+
+Change the modified time of the file in the gulp pipeline based on related dependencies.
+
+## Usage
+
+```js
+const gulp = require('gulp');
+const changed = require('gulp-vtouch');
+const sass = require('gulp-sass'); 
+
+gulp.task('default', () => {
+  const dest = 'dest/style'
+
+  return gulp.src(['src/style/*.scss'])
+    .pipe(vtouch('src/style/*/**/*.scss'))
+    .pipe(changed(dest))
+    .pipe(sass())
+    .pipe(gulp.dest(dest));
+});
+```
+## TODO
+
+Add support for custom rules
+
+Glob variables to vary glob by file
+
+Tests :-(
+
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+var gutil = require('gulp-util');
+var through = require('through2');
+var gs = require("glob-stream");
+var Q = require('kew');
+
+function getLatestDependentTime(glob, opts, latestTimeDeferred) {
+  var latestTime = 0
+
+  gs.create(glob, opts)
+    .on('data', function (file) {
+      var curTime = fs.statSync(file.path).mtime
+      if (curTime > latestTime) {
+        latestTime = curTime
+      }
+    })
+    .on('end', function() {
+      latestTimeDeferred.resolve(latestTime)
+    })
+
+}
+
+module.exports = function (depglob, opts) {
+  opts = opts || {};
+  opts.nosort = true;
+
+  var latestTimeDeferred = Q.defer()
+
+  getLatestDependentTime(depglob, opts, latestTimeDeferred)
+
+  return through.obj(function (file, enc, cb) {
+    function modifyFileTimeIfEarlier(latestTime) {
+      if (file.stat.mtime < latestTime) {
+        file.stat.mtime = latestTime;
+      }
+
+      cb(null, file);
+    }
+
+    latestTimeDeferred.promise
+      .then(modifyFileTimeIfEarlier);
+  });
+};
+
